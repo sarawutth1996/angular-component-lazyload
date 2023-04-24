cc57d7dd9b9e95d18108f052a76884597a878741
sarawutth1996 <sarawut_aum-1145@hotmail.com>
2023-04-24 21:32:54 +0700

    update : create a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
